31d9d87287a5efabca3cdd0a9d7f8a8928c3e9de
petitechose-audio <contact@petitechose.audio>
2026-01-21 11:00:53 +0100

    refactor: update for oc::type namespace
    Adapt to framework breaking change: types moved to oc::type namespace.

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ public:
         .midi = true
     };
 
-    oc::Result<void> init() override {
+    oc::type::Result<void> init() override {
         // Bind each encoder to a MIDI CC
         for (uint8_t i = 0; i < Config::ENCODERS.size(); ++i) {
             uint8_t id = Config::ENCODERS[i].id;
@@ -73,7 +73,7 @@ public:
                 OC_LOG_DEBUG("Encoder: CC {} = {}", cc, midiValue);
             });
         }
-        return oc::Result<void>::ok();
+        return oc::type::Result<void>::ok();
     }
 
     void update() override {}
